633a9fa017ff86777acf9bfd71f7b2327e174c22
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-27 15:44:12 -0500

    resolve survey conflict

--- app/controllers/survey.rb
+++ app/controllers/survey.rb
@@ -8,30 +8,30 @@
 
 # POSTS #
 
-# post "/new_survey" do 
-#   new_survey = Survey.create(params[:survey])
-#     if new_survey.valid? 
-#       session[:survey_id] = new_survey.id
-#       num_questions = params[:num_questions]
-#         num_questions.times do 
-#           question = Question.create
-#           new_survey.questions << question
-#             4.times
-#               response = Response.create
-#               question.responses << response
-#             end
-#         end
-#         redirect "/create_survey"
-#     else
-#       redirect "/user_profile"
-#     end
-# end
+post "/new_survey" do 
+  new_survey = Survey.create(params[:survey])
+    if new_survey.valid? 
+      session[:survey_id] = new_survey.id
+      num_questions = params[:num_questions]
+        num_questions.times do 
+          question = Question.create
+          new_survey.questions << question
+            4.times
+              response = Response.create
+              question.responses << response
+            end
+        end
+        redirect "/create_survey"
+    else
+      redirect "/user_profile"
+    end
+end
 
 
-# post "/build_survey" do
-#   survey_id = session[:survey_id]
-#   survey = Survey.find(survey_id)
-#   survey.questions.each do |question|
-#     question.
-#   end
-# end 
+post "/build_survey" do
+  survey_id = session[:survey_id]
+  survey = Survey.find(survey_id)
+  survey.questions.each do |question|
+    question.
+  end
+end 
