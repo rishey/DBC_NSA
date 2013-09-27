e18ef23438f0f063a17b419881094d352b758930
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-27 18:36:28 -0500

    profile ajax

--- public/js/application.js
+++ public/js/application.js
@@ -4,4 +4,31 @@ $(document).ready(function() {
   // when we try to bind to them
 
   // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+    $('a#sign-out').on("click", function (e) {
+    e.preventDefault();
+    var request = $.ajax({ url: $(this).attr('href'), type: 'delete' });
+    request.done(function () { window.location = "/"; });
+  });
+    $('#new_survey').click(function (e) {
+      e.preventDefault();
+      $(this).hide();
+        var url = $(this).attr('href');
+
+        $.get(url, function(response){
+          $('.container').append(response);
+        });
+
+    });
+    $('.container').on('submit', '#new_survey_form', function(e) {
+      e.preventDefault();
+
+      var url = $(this).attr('action');
+      var data = $(this).serialize();
+
+      $.post(url, data, function(response) {
+        $('tbody').append(response);
+      });
+      $(this).remove();
+      $('#new_survey').show();
+    });
 });
